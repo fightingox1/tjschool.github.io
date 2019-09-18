1ef103c9bbaf708711939f0729e7c20434ee6646
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-09-18 07:21:41 -0700

    Update script.js
    newer versions only use ' when it is a value with one charecter it can use "  whenever so i always do " even if it is only 1 charecter my code had the same problem

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var respond = document.getElementById('respond');
+var respond = document.getElementById("respond");
 function message(message, yes, no) {
 if(confirm(message) == true){
 respond.innerHTML = yes;
